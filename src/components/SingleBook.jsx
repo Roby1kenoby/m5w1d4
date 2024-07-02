ad16769e7a4f72e4fc042a373777edd924b13458
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-07-02 19:16:41 +0200

    commented setSelected function and added inline function

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -5,15 +5,16 @@ import { useState } from 'react';
 function SingleBook({book}) {
     const [selected, setSelected] = useState(false)
     // funzione per fare il toggle della classe selected
-    // non posso mettere direttamente setSelected(!selected) come onClick perchè se no si pianta
-    // react che va in un loop di rerendering
-    const toggleSelected = function(){
-        setSelected(!selected)
-    }
+    // diventa superflua se inserisco una funzione anonima direttamente nell'evento onClick.
+    // quando dichiaro la funzione anonima nell'onclick è come se stessi mettendo solo il nome della funzione
+    // (come se fosse senza parentesi). Di conseguenza, react non la chaima al caricamento, ma solo su onClick
+    // const toggleSelected = function(){
+    //     setSelected(!selected)
+    // }
     // nella card, se selected, allora c'è anche la classe, altrimenti no.
     return (
         <Col lg={3} md={4} sm={6} className='myCol' >
-                <Card className={selected && 'selected'} onClick={toggleSelected}>
+                <Card className={selected && 'selected'} onClick={() => setSelected(!selected)}>
                     <div className='imgWrapper'>    
                         <Card.Img variant="top" src={book.img} />
                     </div>
